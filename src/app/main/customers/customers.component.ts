371f0952d034255e373be2a8861e954d2270c31a
GoldenstarUpwork <juvencocastro@gmail.com>
2018-04-23 12:00:45 +0300

    feat<customers> displayed customers

--- src/app/main/customers/customers.component.ts
+++ src/app/main/customers/customers.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { CustomersService } from '../core/services/customers.service';
+
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss']
 })
+
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private customersServices: CustomersService
+  ) {
   }
 
+  customersList$: Observable<any>;
+  columnsToDisplay: Array<string>;
+
+  ngOnInit() {
+    this.columnsToDisplay = ['id', 'customer_name', 'address', 'phone'];
+
+    this.customersList$ = this.customersServices.getCustomers()
+      .map(customer => customer);
+  }
 }
