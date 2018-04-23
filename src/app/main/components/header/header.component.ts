eae540f522cc0685f13db41295faf2ec2c3a2bc2
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-04-23 16:04:47 +0300

    feat<header> displayed invoices total amount

--- src/app/main/components/header/header.component.ts
+++ src/app/main/components/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/map';
+
+import { InvoicesService } from '../../core/services/invoices.service';
+
 
 @Component({
   selector: 'app-header',
@@ -8,10 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  invoicesTotalAmount: number;
+  invoicesAmount$: Subscription;
   navItems: {
     path: string,
     label: string
   }[];
+
+  constructor(
+    private invoicesServices: InvoicesService,
+  ) {}
+
 
   ngOnInit() {
     this.navItems = [
@@ -19,5 +31,8 @@ export class HeaderComponent implements OnInit {
       { path: '/customers', label: 'Customers'},
       { path: '/invoices', label: 'Invoices'},
     ];
+
+    this.invoicesAmount$ = this.invoicesServices.getInvoices()
+      .subscribe(invoices => this.invoicesTotalAmount = invoices.length);
   }
 }
