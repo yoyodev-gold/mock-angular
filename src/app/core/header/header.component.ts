fe054a8bbaf656881e2ca02970cf35e90373aaaf
GoldenstarUpwork <juvencocastro@gmail.com>
2018-04-26 17:13:07 +0300

    feat<header-service;invoices> added hideInkBar function onclick

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 import { Subscription } from 'rxjs/Subscription';
+import { MatTabLink } from '@angular/material';
 
 import { InvoicesService } from '../services/invoices.service';
+import { HeaderService } from '../services/header.service';
 
 
 @Component({
@@ -10,20 +12,21 @@ import { InvoicesService } from '../services/invoices.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   
-  hidden;
   invoicesTotalAmount;
   invoicesAmount$: Subscription;
   navItems: {
     path: string,
     label: string
   }[];
+  @ViewChildren(MatTabLink) inkBar: QueryList<any>;
 
   constructor(
     private invoicesServices: InvoicesService,
+    private headerService: HeaderService,
+    private renderer: Renderer2,
   ) {}
-
 
   ngOnInit() {
     this.navItems = [
@@ -34,5 +37,13 @@ export class HeaderComponent implements OnInit {
 
     this.invoicesAmount$ = this.invoicesServices.getInvoices()
       .subscribe(invoices => this.invoicesTotalAmount = invoices.length);
+  }
+
+  ngAfterViewInit() {
+    this.headerService.inkBar = this.inkBar;
+  }
+
+  hideInkBar() {
+    this.headerService.hideInkBar();
   }
 }
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,10 +1,12 @@
 import { InvoicesService } from './invoices.service';
 import { CustomersService } from './customers.service';
 import { ProductsService } from './products.service';
+import { HeaderService } from './header.service';
 
 
 export const APP_SERVICE_PROVIDERS = [
   InvoicesService,
   CustomersService,
   ProductsService,
+  HeaderService
 ];
