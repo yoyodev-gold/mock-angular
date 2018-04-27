d3edc2d1ef63a0f95a500b531373ad4fc96d5c6d
GoldenstarUpwork <juvencocastro@gmail.com>
2018-04-27 20:18:45 +0300

    fixed<products> converted cold Observable to connectableObservable(draft)

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -2,21 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import { shareReplay } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/mergeAll';
+import 'rxjs/add/operator/publishReplay';
 
 import { Product } from '../interfaces/product';
+import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
 
 
 @Injectable()
 export class ProductsService {
 
   productsList$: Observable<Product[]>;
+  productsStream$: ConnectableObservable<Product[]>;
+  passRequest: Subject<Observable<Product[]>>;
 
-  constructor( private httpClient: HttpClient) {}
+  constructor( private httpClient: HttpClient) {
+    this.passRequest = new Subject();
+    this.productsStream$ = this.passRequest.mergeAll().publishReplay(1);
+    this.productsStream$.connect();
+
+    this.productsList$ = this.productsStream$.pipe(
+      map( product => product )
+    );
+  }
 
   getProducts() {
-    return this.productsList$ = this.httpClient.get<Product[]>('products').pipe(
-      shareReplay(1)
-    );
+    this.passRequest.next(this.httpClient.get<Product[]>('products'));
+    return this.productsList$;
+    //return this.productsList$ = this.httpClient.get<Product[]>('products').pipe(
+    //  shareReplay(1)
+    //);
   }
 }
