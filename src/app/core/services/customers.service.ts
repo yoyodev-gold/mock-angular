585dea3ddaafd7fb122dd1c37acf77d74651413e
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-07-14 16:56:31 +0300

    fix(core): change customers to connectable observable

--- src/app/core/services/customers.service.ts
+++ src/app/core/services/customers.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import { shareReplay } from 'rxjs/operators';
+import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
+import { Subject } from 'rxjs/Subject';
+import { mergeScan} from 'rxjs/operators';
+import 'rxjs/add/operator/publishReplay';
+import 'rxjs/add/observable/of';
 
 import { Customer } from '../interfaces/customer';
 
@@ -10,16 +14,29 @@ import { Customer } from '../interfaces/customer';
 @Injectable()
 export class CustomersService {
 
-  customersList$: Observable<Customer[]>;
+  customersList$: ConnectableObservable<Customer[]>;
+  passRequest: Subject<Observable<Customer[]>>;
 
-  constructor( private httpClient: HttpClient ) {}
+  constructor(
+    private httpClient: HttpClient,
+  ) {
+    this.passRequest = new Subject();
+    this.customersList$ = this.passRequest.pipe(
+      mergeScan((acc) => acc ? Observable.of(acc) : this.getCustomersRequest(), null)
+    ).publishReplay(1);
+    this.customersList$.connect();
+  }
+
+  getCustomersRequest() {
+    return this.httpClient.get<Customer[]>('customers');
+  }
 
   getCustomers() {
-    return this.customersList$ = this.httpClient.get<Customer[]>('customers').pipe(
-      shareReplay(1)
-    );
+    this.passRequest.next();
+    return this.customersList$;
   }
+
   getCustomer(id) {
-      return this.httpClient.get<Customer[]>(`customers/${id}`);
+    return this.httpClient.get<Customer[]>(`customers/${id}`);
   }
 }
