0a248ac3e602201607f5e6ac6fc2e0e378fc68ea
GoldenstarUpwork <juvencocastro@gmail.com>
2018-07-14 16:56:31 +0300

    fix(core): change customers to connectable observable

--- src/app/core/resolvers/customers-resolver.ts
+++ src/app/core/resolvers/customers-resolver.ts
@@ -2,23 +2,21 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
+import { take } from 'rxjs/operators';
 
 import { Customer } from '../interfaces/customer';
 import { CustomersService } from '../services/customers.service';
 
 @Injectable()
 export class CustomersResolver implements Resolve<Customer[]> {
-
   constructor(private customersService: CustomersService) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any>|Promise<any>|any {
-    if (this.customersService.customersList$) {
-      return this.customersService.customersList$;
-    } else {
-      this.customersService.getCustomers();
-    }
+      return this.customersService.getCustomers().pipe(
+        take(1)
+      );
   }
 }
