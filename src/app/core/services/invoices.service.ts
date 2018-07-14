ca6172a2b24e99e9c4f1c0b5eaa27974f3215fac
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-07-14 17:09:07 +0300

    fix(invoices): change invoices to connectable observable

--- src/app/core/services/invoices.service.ts
+++ src/app/core/services/invoices.service.ts
@@ -2,23 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import { shareReplay } from 'rxjs/operators';
+import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
+import { Subject } from 'rxjs/Subject';
+import { mergeScan} from 'rxjs/operators';
+import 'rxjs/add/operator/publishReplay';
+import 'rxjs/add/observable/of';
 
 import { Invoice } from '../interfaces/invoice';
+import { Product } from '../interfaces/product';
 
 
 @Injectable()
 export class InvoicesService {
 
-  invoicesList$: Observable<Invoice[]>;
+  invoicesList$: ConnectableObservable<Invoice[]>;
+  passRequest: Subject<Observable<Invoice[]>>;
 
-  constructor( private httpClient: HttpClient ) {}
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.passRequest = new Subject();
+    this.invoicesList$ = this.passRequest.pipe(
+      mergeScan((acc) => acc ? Observable.of(acc) : this.getInvoicesRequest(), null)
+    ).publishReplay(1);
+    this.invoicesList$.connect();
+  }
+
+  getInvoicesRequest() {
+    return this.httpClient.get<Product[]>('invoices');
+  }
 
   getInvoices() {
-    return this.invoicesList$ = this.httpClient.get<Invoice[]>('invoices').pipe(
-      shareReplay(1)
-    );
+    this.passRequest.next();
+    return this.invoicesList$;
   }
+
   getInvoice(id) {
     return this.httpClient.get<Invoice[]>(`invoices/${id}`);
   }
