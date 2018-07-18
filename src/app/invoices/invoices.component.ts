47bd1aa1a84284e30c70482af4514eafb6d54e37
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-07-18 23:04:54 +0300

    feat(invoices): create new stream for updating invoices collection after deleteing specific invoice(draft)

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { combineLatest } from 'rxjs/observable/combineLatest';
-import { map } from 'rxjs/operators';
+import { map, switchMap, mergeAll, tap } from 'rxjs/operators';
 
 import { Invoice } from '../core/interfaces/invoice';
 import { InvoicesService } from '../core/services/invoices.service';
 import { CustomersService } from '../core/services/customers.service';
 import { HeaderService } from '../core/services/header.service';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/observable/merge';
+
 
 
 @Component({
@@ -17,28 +20,45 @@ import { HeaderService } from '../core/services/header.service';
 })
 export class InvoicesComponent implements OnInit {
 
-  invoicesList$: Observable<Invoice[]>;
+  invoicesList$;
+  deleteInvoice$;
+  invoicesListData$: Observable<Invoice[]>;
+  deleteCurrentInvoice$: Subject<number> = new Subject();
   columnsToDisplay: Array<string>;
 
   constructor(
     private invoicesServices: InvoicesService,
     private customersServices: CustomersService,
     private headerService: HeaderService,
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.columnsToDisplay = ['number', 'id', 'customer_name', 'discount', 'total', 'actions'];
 
-    this.invoicesList$ = combineLatest(this.invoicesServices.invoicesList$, this.customersServices.customersList$).pipe(
+    this.invoicesListData$ = combineLatest(this.invoicesServices.invoicesList$, this.customersServices.customersList$).pipe(
       map(([invoices, customers]) => {
         return invoices.map(invoice => {
           invoice.customer = customers.find(customer => invoice.customer_id === customer.id);
-            return invoice;
+          return invoice;
         });
-      })
+      }),
+    );
+
+    this.deleteInvoice$ = this.deleteCurrentInvoice$.pipe(
+      switchMap(id => this.invoicesServices.deleteInvoice(id))
+    );
+
+    this.invoicesList$ = Observable.merge(
+      this.invoicesListData$,
+      this.deleteInvoice$,
     );
   }
+
   hideInkBar() {
     this.headerService.hideInkBar();
+  }
+  deleteInvoice(id) {
+    this.deleteCurrentInvoice$.next(id);
   }
 }
