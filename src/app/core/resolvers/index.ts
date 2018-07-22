519ff74f42d25ad4ef61748c1313a7b82b85a361
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-07-22 21:25:03 +0300

    feat(core:resolvers): create view-invoice resolver

--- src/app/core/resolvers/index.ts
+++ src/app/core/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { InvoicesResolver } from './invoices-resolver';
 import { CustomersResolver } from './customers-resolver';
 import { ProductsResolver } from './products-resolver';
+import { ViewInvoiceResolver } from './view-invoice-resolver';
 
 export const APP_RESOLVERS_PROVIDERS = [
   InvoicesResolver,
   CustomersResolver,
   ProductsResolver,
+  ViewInvoiceResolver,
 ];
