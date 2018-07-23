f42d35a8dd2259a73f4bb6fb2110d4ced61526ed
GoldenstarUpwork <juvencocastro@gmail.com>
2018-07-23 23:39:17 +0300

    feat(view-invoice) create new stream for displaying specific invoice data(draft-items are not updated

--- src/app/view-invoice/view-invoice.component.ts
+++ src/app/view-invoice/view-invoice.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { combineLatest } from 'rxjs/observable/combineLatest';
 
 import { InvoicesService } from '../core/services/invoices.service';
 import { CustomersService } from '../core/services/customers.service';
 import { ProductsService } from '../core/services/products.service';
 import { Observable } from 'rxjs/Observable';
+import * as _ from 'lodash';
+
 
 import { Invoice } from '../core/interfaces/invoice';
 
@@ -17,7 +20,11 @@ import { Invoice } from '../core/interfaces/invoice';
   styleUrls: ['./view-invoice.component.scss']
 })
 export class ViewInvoiceComponent implements OnInit {
-  invoice$: Observable<Invoice>;
+  invoice$;
+  updatedInvoice$;
+  currentInvoice$;
+  invoiceItems$;
+  columnsToDisplay: Array<string>;
 
   constructor(
       private route: ActivatedRoute,
@@ -28,14 +35,35 @@ export class ViewInvoiceComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.invoice$ = this.route.paramMap.pipe(
+    this.columnsToDisplay = ['number', 'product_name', 'quantity', 'price'];
+
+    this.currentInvoice$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
-        return this.invoicesService.invoicesList$.pipe(
+        return this.invoicesService.invoicesCollection$.pipe(
           map(invoices => {
             return invoices.find(invoice => invoice.id === +params.get('id'));
-          }));
+          }),
+        );
       })
     );
+    this.invoiceItems$ = this.invoicesService.invoicesItemsList$;
+
+    this.updatedInvoice$ = combineLatest(
+      this.productsService.productsList$,
+      this.invoiceItems$,
+      this.currentInvoice$,
+    ).pipe(
+      map(([products, invoiceItems, currentInvoice]) => {
+        currentInvoice.items = _.map(invoiceItems, item => {
+          return {
+            ...item,
+            product: products.find(product => item.product_id === product.id),
+          };
+        });
+        return currentInvoice;
+      }),
+    ).publishReplay(1);
+    this.updatedInvoice$.connect();
   }
 
   getCombinedData(id) {
