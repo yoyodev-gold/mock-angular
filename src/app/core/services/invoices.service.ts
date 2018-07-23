d6aa12b8eba966095a51cbcbed596ab05949add7
GoldenstarUpwork <juvencocastro@gmail.com>
2018-07-23 23:43:10 +0300

    feat(core:invoices): add request and stream for gettin specific invoice-item(draft)

--- src/app/core/services/invoices.service.ts
+++ src/app/core/services/invoices.service.ts
@@ -5,24 +5,28 @@ import { Observable } from 'rxjs/Observable';
 import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
 import { Subject } from 'rxjs/Subject';
 import { combineLatest } from 'rxjs/observable/combineLatest';
-import { map, mergeScan, switchMap } from 'rxjs/operators';
+import { map, mergeScan, switchMap, tap } from 'rxjs/operators';
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/merge';
 import * as _ from 'lodash';
 
 import { Invoice } from '../interfaces/invoice';
-import { Product } from '../interfaces/product';
+import { InvoiceItem } from '../interfaces/invoice-item';
 import { CustomersService } from './customers.service';
 
 
 @Injectable()
 export class InvoicesService {
 
+  passRequest: Subject<Observable<Invoice[]>> = new Subject();
   invoicesList$: ConnectableObservable<Invoice[]>;
   invoicesListCombined$: Observable<Invoice[]>;
   invoicesCollection$: ConnectableObservable<Invoice[]>;
-  passRequest: Subject<Observable<Invoice[]>> = new Subject();
+
+  passItemsRequest: Subject<Observable<InvoiceItem[]>> = new Subject();
+  invoicesItemsList$: ConnectableObservable<InvoiceItem[]>;
+
   deleteInvoice$: Subject<number> = new Subject();
   deleteInvoiceSubscription$: Observable<Invoice[]>;
 
@@ -35,6 +39,11 @@ export class InvoicesService {
       mergeScan((acc) => acc ? Observable.of(acc) : this.getInvoicesRequest(), null),
     ).publishReplay(1);
     this.invoicesList$.connect();
+
+    this.invoicesItemsList$ = this.passItemsRequest.pipe(
+      mergeScan((acc, id) => (acc) ? Observable.of(acc) : this.getInvoiceItemsRequest(id), null),
+    ).publishReplay(1);
+    this.invoicesItemsList$.connect();
 
     this.invoicesListCombined$ = combineLatest(this.invoicesList$, this.customersService.customersList$).pipe(
       map(([invoices, customers]) => {
@@ -54,6 +63,8 @@ export class InvoicesService {
     this.invoicesCollection$ = Observable.merge(
       this.invoicesListCombined$,
       this.deleteInvoiceSubscription$,
+    ).pipe(
+      tap(console.error)
     ).publishReplay(1);
     this.invoicesCollection$.connect();
   }
@@ -62,9 +73,17 @@ export class InvoicesService {
     return this.httpClient.get<Invoice[]>('invoices');
   }
 
+  getInvoiceItemsRequest(id) {
+    return this.httpClient.get<InvoiceItem[]>(`invoices/${id}/items`);
+  }
+
   getInvoices() {
     this.passRequest.next();
     return this.invoicesList$;
+  }
+  getInvoiceItems(id) {
+    this.passItemsRequest.next(id);
+    return this.invoicesItemsList$;
   }
 
   deleteInvoice(id) {
