cb4b1068285c13eb7d49396e46d59c0f3bebc1e8
GoldenstarUpwork <juvencocastro@gmail.com>
2018-07-31 22:20:36 +0300

    style(view-invoice): add typization and correct imports structure

--- src/app/view-invoice/view-invoice.component.ts
+++ src/app/view-invoice/view-invoice.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
+import { Observable } from 'rxjs/Observable';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { combineLatest } from 'rxjs/observable/combineLatest';
 
 import { InvoicesService } from '../core/services/invoices.service';
 import { CustomersService } from '../core/services/customers.service';
 import { ProductsService } from '../core/services/products.service';
-import { Observable } from 'rxjs/Observable';
 import * as _ from 'lodash';
 
-
+import { InvoiceItem } from '../core/interfaces/invoice-item';
 import { Invoice } from '../core/interfaces/invoice';
 
 
@@ -20,10 +20,10 @@ import { Invoice } from '../core/interfaces/invoice';
   styleUrls: ['./view-invoice.component.scss']
 })
 export class ViewInvoiceComponent implements OnInit {
-  invoice$;
-  updatedInvoice$;
-  currentInvoice$;
-  invoiceItems$;
+
+  updatedInvoice$: Observable<Invoice>;
+  currentInvoice$: Observable<Invoice>;
+  invoiceItems$: Observable<InvoiceItem[]>;
   columnsToDisplay: Array<string>;
 
   constructor(
@@ -62,7 +62,6 @@ export class ViewInvoiceComponent implements OnInit {
         });
         return currentInvoice;
       }),
-    ).publishReplay(1);
-    this.updatedInvoice$.connect();
+    );
   }
 }
