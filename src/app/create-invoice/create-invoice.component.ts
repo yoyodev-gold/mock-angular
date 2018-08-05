10ab17384addf55257209689304c9ba4655d3a9f
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-08-05 23:41:29 +0300

    feat(create-invoice): add customer's and product's data streams

--- src/app/create-invoice/create-invoice.component.ts
+++ src/app/create-invoice/create-invoice.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+import { Observable } from 'rxjs/Observable';
+
+import { CustomersService } from '../core/services/customers.service';
+import { ProductsService } from '../core/services/products.service';
+
+import { Customer } from '../core/interfaces/customer';
+import { Product } from '../core/interfaces/product';
+
 
 @Component({
   selector: 'app-create-invoice',
@@ -10,12 +18,19 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class CreateInvoiceComponent implements OnInit {
 
   newInvoice: FormGroup;
+  customersList$: Observable<Customer[]>;
+  productsList$: Observable<Product[]>;
 
   constructor(
+    private customerService: CustomersService,
+    private productsService: ProductsService,
   ) {
   }
 
   ngOnInit() {
+    this.customersList$ = this.customerService.customersList$;
+    this.productsList$ = this.productsService.productsList$;
+
     this.newInvoice = new FormGroup({
       name: new FormControl(),
       product: new FormControl(),
