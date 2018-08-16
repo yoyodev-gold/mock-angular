fd6734d6635caf619b6b1c4741dea1918bfcab49
GoldenstarUpwork <juvencocastro@gmail.com>
2018-08-16 23:28:40 +0300

    feat(shared): create modals folder and modal-box module

---
+++ src/app/shared/modals/modal-box.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+} from '@angular/core';
+
+
+@Component({
+  selector: 'app-modal-box',
+  templateUrl: './modal-box.component.html',
+  styleUrls: ['./modal-box.component.scss']
+})
+
+export class ModalBoxComponent {
+
+  constructor(
+  ) {
+  }
+
+}
---
+++ src/app/shared/modals/modal-box.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ModalBoxComponent } from './modal-box.component';
+
+@NgModule({
+  declarations: [ ModalBoxComponent ],
+  imports: [
+  ]
+})
+
+export class ModalBoxModule { }
