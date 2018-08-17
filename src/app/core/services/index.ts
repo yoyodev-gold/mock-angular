b2a291dd4d99431252e307eba285267c55e1d957
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-08-17 21:58:56 +0300

    feat(core:services): import modal-box service

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,11 +2,13 @@ import { InvoicesService } from './invoices.service';
 import { CustomersService } from './customers.service';
 import { ProductsService } from './products.service';
 import { HeaderService } from './header.service';
+import { ModalBoxService } from './modal-box.service';
 
 
 export const APP_SERVICE_PROVIDERS = [
   InvoicesService,
   CustomersService,
   ProductsService,
-  HeaderService
+  HeaderService,
+  ModalBoxService
 ];
