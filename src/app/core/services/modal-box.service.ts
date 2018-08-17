e66dd21d09ae8cd63286ff26894d82d2004f6263
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-08-17 23:05:22 +0300

    feat(core:services) add basic logic to modal-box service

--- src/app/core/services/modal-box.service.ts
+++ src/app/core/services/modal-box.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { MatDialog } from '@angular/material';
+
+import { ModalBoxComponent } from '../modal-box/modal-box.component';
+
 
 @Injectable()
 export class ModalBoxService {
 
   constructor(
-  ) {
+    private dialog: MatDialog
+  ) {}
+
+  confirmModal(message: string, twoBtn: boolean = true) {
+    return this.dialog.open(
+      ModalBoxComponent,
+      {
+        data: {message: message, twoBtn: twoBtn},
+      }
+    )
+    .afterClosed();
   }
 }
