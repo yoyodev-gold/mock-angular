d9343b405fa21cf6769bceccb355f9cd066eb841
GoldenstarUpwork <juvencocastro@gmail.com>
2018-08-17 23:04:01 +0300

    feat(core:modal-box) add basic logic

--- src/app/core/modal-box/modal-box.component.ts
+++ src/app/core/modal-box/modal-box.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
+  Inject,
 } from '@angular/core';
 
+import { MAT_DIALOG_DATA , MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-modal-box',
@@ -10,9 +12,18 @@ import {
 })
 
 export class ModalBoxComponent {
+  message: string;
+  twoBtn: boolean;
 
   constructor(
+    public dialogRef: MatDialogRef<ModalBoxComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {
+    this.message = this.data.message;
+    this.twoBtn = this.data.twoBtn;
   }
 
+  choose(choice: boolean) {
+    this.dialogRef.close(choice);
+  }
 }
