1b7c760087ea3d2b9164bf1d2d056b81db79e71e
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-10-21 19:04:44 +0300

    refactor(invoices): transfer delete invoice modal window logic to a service

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import { Subject } from 'rxjs/Subject';
-import { filter, mapTo, mergeMap, switchMap, tap } from 'rxjs/operators';
 
 import { Invoice } from '../core/interfaces/invoice';
 
 import { InvoicesService } from '../core/services/invoices.service';
 import { CustomersService } from '../core/services/customers.service';
 import { HeaderService } from '../core/services/header.service';
-import { ModalBoxService } from '../core/services/modal-box.service';
 
 
 @Component({
@@ -19,17 +15,14 @@ import { ModalBoxService } from '../core/services/modal-box.service';
   styleUrls: ['./invoices.component.scss']
 })
 export class InvoicesComponent implements OnInit {
-  columnsToDisplay: Array<string>;
 
+  columnsToDisplay: Array<string>;
   invoicesCollection$: Observable<Invoice[]>;
-  deleteInvoice$: Subject<any> = new Subject<any>();
-  deleteInvoiceSubscription: Subscription;
 
   constructor(
     private invoicesService: InvoicesService,
     private customersService: CustomersService,
     private headerService: HeaderService,
-    private modalBoxService: ModalBoxService,
   ) {
   }
 
@@ -37,16 +30,6 @@ export class InvoicesComponent implements OnInit {
     this.columnsToDisplay = ['number', 'id', 'customer_name', 'discount', 'total', 'actions'];
 
     this.invoicesCollection$ = this.invoicesService.invoicesCollection$;
-
-    this.deleteInvoiceSubscription = this.deleteInvoice$.pipe(
-      mergeMap(id => this.modalBoxService.confirmModal('Are you sure you want to delete an invoice?').pipe(
-          filter(choice => !!choice),
-          mapTo(id)
-        )
-      ),
-      switchMap(id => this.invoicesService.deleteInvoiceRequest(id)),
-      tap(invoices => this.modalBoxService.confirmModal(`Invoice number ${invoices.id} has been deleted`, false)),
-    ).subscribe();
   }
 
   hideInkBar() {
@@ -54,6 +37,6 @@ export class InvoicesComponent implements OnInit {
   }
 
   deleteInvoice(id) {
-    this.deleteInvoice$.next(id);
+    this.invoicesService.deleteInvoiceOpenModal$.next(id);
   }
 }
