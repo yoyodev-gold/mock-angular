2d9fd7b7b206a4da42188e58294e2476a17bea48
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-10-22 23:37:14 +0300

    feat(create-edit-ivoice): create invoice-items-group module and import it in parent

---
+++ src/app/create-edit-invoice/invoice-items/invoice-items-group.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-invoice-items-group',
+  templateUrl: './invoice-items-group.component.html',
+  styleUrls: ['./invoice-items-group.component.scss']
+})
+export class InvoiceItemsGroupComponent {
+}
