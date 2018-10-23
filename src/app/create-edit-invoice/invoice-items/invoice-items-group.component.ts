8ae1d0ae294d83fe055134c309b6ded0026f8389
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-10-23 21:05:42 +0300

    feat(create-edit-invoice:invoice-items): add necessary markup and variables

--- src/app/create-edit-invoice/invoice-items/invoice-items-group.component.ts
+++ src/app/create-edit-invoice/invoice-items/invoice-items-group.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
+import { Observable } from 'rxjs/Observable';
+
+import { Product } from '../../core/interfaces/product';
+
+import { ProductsService } from '../../core/services/products.service';
 
 @Component({
   selector: 'app-invoice-items-group',
   templateUrl: './invoice-items-group.component.html',
   styleUrls: ['./invoice-items-group.component.scss']
 })
-export class InvoiceItemsGroupComponent {
+export class InvoiceItemsGroupComponent implements OnInit {
+  @Input('itemsGroup') itemsGroup: FormGroup;
+  @Input('groupIndex') groupIndex: number;
+
+  productsList$: Observable<Product[]>;
+
+  constructor(
+    private productsService: ProductsService
+  ) {
+  }
+
+  get createInvoiceQuantityControl() {
+    return this.itemsGroup.get('quantity');
+  }
+  get createInvoicePriceControl() {
+    return this.itemsGroup.get('price');
+  }
+
+  ngOnInit() {
+    this.productsList$ = this.productsService.productsList$;
+  }
 }
