ffec138c75490ccda0d8832316f9612fc65753cd
GoldenstarUpwork <juvencocastro@gmail.com>
2018-10-24 23:41:19 +0300

    style(create-edit-invoice): remove commented and useless code

--- src/app/create-edit-invoice/create-edit-invoice.component.ts
+++ src/app/create-edit-invoice/create-edit-invoice.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
-import * as _ from 'lodash';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { Subject } from 'rxjs/Subject';
@@ -28,12 +27,14 @@ export class CreateEditInvoiceComponent implements OnInit, OnDestroy {
   customersList$: Observable<Customer[]>;
   productsList$: Observable<Product[]>;
   invoicesList$: Observable<Invoice[]>;
-  createInvoiceFormSubscription: Subscription;
+
   totalControlSubscription: Subscription;
+  discountControlSubscription: Subscription;
   createInvoiceSubscription: Subscription;
+
   passCreateInvoiceRequest$: Subject<any> = new Subject();
   arrayAmount: BehaviorSubject<number> = new BehaviorSubject(0);
-  discountControlSubscription;
+
   constructor(
     private customerService: CustomersService,
     private productsService: ProductsService,
@@ -56,21 +57,6 @@ export class CreateEditInvoiceComponent implements OnInit, OnDestroy {
     this.productsList$ = this.productsService.productsList$;
     this.invoicesList$ = this.invoicesService.invoicesCollection$;
 
-    // this.createInvoiceForm = new FormGroup({
-    //   customer_id: new FormControl(null, Validators.required ),
-    //   product_id: new FormControl(null, Validators.required),
-    //   quantity: new FormControl(null, {
-    //     validators: Validators.required,
-    //     updateOn: 'blur'
-    //   }),
-    //   price: new FormControl(null),
-    //   discount: new FormControl(null, {
-    //     validators: Validators.required,
-    //     updateOn: 'blur'
-    //   }),
-    //   total: new FormControl(),
-    // });
-
     this.createInvoiceForm = new FormGroup({
       customer_id: new FormControl(null),
       discount: new FormControl(null, {
@@ -83,19 +69,6 @@ export class CreateEditInvoiceComponent implements OnInit, OnDestroy {
     this.createInvoiceItemsArray.push(this.fillInvoiceArray());
     this.createInvoiceItemsArray.push(this.fillInvoiceArray());
     this.createInvoiceItemsArray.push(this.fillInvoiceArray());
-
-    // this.createInvoiceFormSubscription = this.createInvoiceForm.valueChanges.pipe(
-    //   filter(form =>  form.quantity && form.product_id),
-    // ).subscribe(form => {
-    //     const total = (+form.quantity * form.price) * ((100 - +form.discount) / 100);
-    //     return this.createInvoiceTotalControl.patchValue(total, {onlySelf: true});
-    //   }
-    // );
-    // this.productControlSubscription = this.createInvoiceProductControl.valueChanges.pipe(
-    //   switchMap( productName => this.productsList$.pipe(
-    //     map(products => _.find(products, {'id': productName}).price),
-    //   ))
-    // ).subscribe(price => this.createInvoicePriceControl.patchValue(price));
 
     this.totalControlSubscription = this.createInvoiceItemsArray.valueChanges.pipe(
       filter(items => !!items.filter(item => item.price && item.quantity).length),
@@ -150,8 +123,6 @@ export class CreateEditInvoiceComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    // this.createInvoiceFormSubscription.unsubscribe();
-    // this.productControlSubscription.unsubscribe();
     this.totalControlSubscription.unsubscribe();
     this.discountControlSubscription.unsubscribe();
     this.createInvoiceSubscription.unsubscribe();
