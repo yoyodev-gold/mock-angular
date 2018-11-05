8e2b426b1877b5802fe4aa68ce5477734dc54b07
GoldenstarUpwork <juvencocastro@gmail.com>
2018-11-06 00:46:31 +0200

    style(core:services): update to a common styleguide

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -4,28 +4,29 @@ import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
 import { Subject } from 'rxjs/Subject';
-import { mergeScan} from 'rxjs/operators';
+import { mergeScan } from 'rxjs/operators';
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/observable/of';
 
 import { Product } from '../interfaces/product';
 
-
 @Injectable()
 export class ProductsService {
+  
+  passProductsRequest: Subject<any> = new Subject();
   productsList$: ConnectableObservable<Product[]>;
-  passRequest: Subject<Observable<Product[]>>;
 
-  constructor( private httpClient: HttpClient) {
-    this.passRequest = new Subject();
-    this.productsList$ = this.passRequest.pipe(
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.productsList$ = this.passProductsRequest.pipe(
       mergeScan((acc) => acc ? Observable.of(acc) : this.getProductsRequest(), null)
     ).publishReplay(1);
     this.productsList$.connect();
-}
+  }
 
   getProducts() {
-    this.passRequest.next();
+    this.passProductsRequest.next();
     return this.productsList$;
   }
 
