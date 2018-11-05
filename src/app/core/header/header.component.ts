be26fe15a31b629b59ad8bdd2bc83ee945aa3778
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-11-06 00:36:57 +0200

    refactor(core:services): add stream for hiding nav-ink-bar instead of header service

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit, QueryList, ViewChildren, } from '@angular/core';
+import { Component, OnDestroy, OnInit, QueryList, ViewChildren, } from '@angular/core';
 
 import { MatTabLink } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Invoice } from '../interfaces/invoice';
 
 import { InvoicesService } from '../services/invoices.service';
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   
   @ViewChildren(MatTabLink) inkBar: QueryList<any>;
   
   invoicesAmount$: Observable<Invoice[]>;
+  hideNavInkBarSubscription: Subscription;
+  
   navItems: { path: string, label: string }[] = [
     { path: '/products', label: 'Products' },
     { path: '/customers', label: 'Customers'},
@@ -31,9 +33,16 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.invoicesAmount$ = this.invoicesService.invoicesCollection$;
+    
+    this.hideNavInkBarSubscription = this.invoicesService.hideNavInkBar$
+      .subscribe(() => this.hideInkBar());
+  }
+  
+  ngOnDestroy() {
+    this.hideNavInkBarSubscription.unsubscribe();
   }
 
   hideInkBar() {
-    this.inkBar.last._elementRef.nativeElement.nextElementSibling.style.visibility = 'hidden' ;
+    this.inkBar.last._elementRef.nativeElement.nextElementSibling.style.visibility = 'hidden';
   }
 }
